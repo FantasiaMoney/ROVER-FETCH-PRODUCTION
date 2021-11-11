52e35566fc4b24caf57182632855499c34099ace
RuslanMirov <ruslan777mirov@gmail.com>
2021-11-11 15:26:05 +0200

    Update with 1000000000 * 100 * 10**6 * 10**9

--- contracts/TOKEN.sol
+++ contracts/TOKEN.sol
@@ -716,7 +716,7 @@ contract TOKEN is Context, IERC20, Ownable {
     address[] private _excluded;
 
     uint256 private constant MAX = ~uint256(0);
-    uint256 private _tTotal = 1000000000 * 10**6 * 10**9;
+    uint256 private _tTotal = 1000000000 * 100 * 10**6 * 10**9;
     uint256 private _rTotal = (MAX - (MAX % _tTotal));
     uint256 private _tFeeTotal;
 
