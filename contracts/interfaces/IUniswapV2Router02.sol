646cb6a8c3b3bfb991bd9f4339921e1d7c50ee42
RuslanMirov <ruslan777mirov@gmail.com>
2021-11-25 16:03:23 +0200

    Update with add split LD from sale

--- contracts/interfaces/IUniswapV2Router02.sol
+++ contracts/interfaces/IUniswapV2Router02.sol
@@ -12,6 +12,15 @@ interface IUniswapV2Router02 {
       uint deadline
   ) external returns (uint amountA, uint amountB, uint liquidity);
 
+  function addLiquidityETH(
+      address token,
+      uint amountTokenDesired,
+      uint amountTokenMin,
+      uint amountETHMin,
+      address to,
+      uint deadline
+  ) external payable returns (uint amountToken, uint amountETH, uint liquidity);
+
   function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
       external
       payable
